b0c39a1d606d362df133e99aed6bf9ac8bd9133b
Prabhat Mishra <103622186+prabhat706@users.noreply.github.com>
2023-05-22 18:16:58 +0530

    Add files via upload
    AREA OF RECTANGLE

---
+++ AreaofRectangle.cpp
@@ -0,0 +1,16 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    int length;
+    cout << "Enter the length value" << endl;
+    cin >> length;
+
+    int width;
+    cout << "Enter the width value" << endl;
+    cin >> width;
+
+    int area = length * width;
+    cout << "Area of rectangle is " << area << endl;
+}
