e45b2dffe87f92e72c5ecab1f4416083f1c3fe82
Prabhat Mishra <103622186+prabhat706@users.noreply.github.com>
2023-05-24 22:16:41 +0530

    Add files via upload
    SWAP TWO NUMBERS CPP

---
+++ SwapTwoNumbers.cpp
@@ -0,0 +1,16 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    int a;
+    cout << "Enter the value of a " << endl;
+    cin >> a;
+
+    int b;
+    cout << "Enter the value of b " << endl;
+    cin >> b;
+
+    swap(a, b);
+    cout << a << " " << b << endl;
+}
